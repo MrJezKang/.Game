ab3d64ca92b0c80c4c9e0cd73ad83eedd05777d5
Jez Kang <markangcog@yahoo.com>
2026-04-05 01:49:59 +0800

    Refractor

--- engine/instance.h
+++ engine/instance.h
@@ -3,13 +3,7 @@
 
 #include <SDL2/SDL.h>
 
-
-struct Entity {
-    float x, y, w, h;
-    float vx = 0, vy = 0;
-    bool isDragging = false;
-};
-/*struct Box {
+struct Box {
     float x = 400, y = 300, w = 50, h = 50;
     float vx = 0, vy = 0; //Variables
     bool isDragging = false;
@@ -24,5 +18,5 @@ struct Rect {
 
 void UpdatePhysics(Box& b);
 void UpdatePhysics(Rect& r);
-*/
+
 #endif
