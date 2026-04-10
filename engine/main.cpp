b07341051c735e20129b5062f05001d5ac1fc5bf
Jez Kang <markangcog@yahoo.com>
2026-04-10 13:03:16 +0800

    Rendering Engine Setup

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,15 +1,19 @@
 #include <SDL2/SDL.h>
+#include <vector>
 
+//default Windowed Scrren Size
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
 int main(int argc, char* argv[]){
 
+    //Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0){
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
 
+    //Window creation
     SDL_Window* window = SDL_CreateWindow(
         "SDL Tutorial", 
         SDL_WINDOWPOS_CENTERED, 
@@ -18,11 +22,44 @@ int main(int argc, char* argv[]){
         SCREEN_HEIGHT, 
         SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
     );
-        
-        SDL_Renderer* renderer = SDL_CreateRenderer(
-            window, 
-            -1,
-            SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
-        );
+
+    if(window == NULL){
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    
+    //Renderer creation
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window, 
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+
+    bool LoopActive = true;
+    SDL_Event e;
+
+
+    //==============================================MAIN LOOP==============================================
+    //==============================================THREAD 1===============================================
+    while(LoopActive){
+
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                LoopActive = false;
+            }
+        }
+
+        SDL_RenderClear(renderer); // Clear the screen with the current drawing color (default is black)
+
+        SDL_SetRenderDrawColor(renderer, rand() % 256, 0, 0, 255);
+
+        SDL_RenderPresent(renderer); // Render the current frame to the screen
+
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 
 }
